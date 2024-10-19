4bd4b7c3f21d11fcc8035bef460ebc3dc41c5b20
Sebastian Giraudo <seba.jose.giraudo@gmail.com>
2024-10-18 23:42:37 -0300

    Change tab name and icon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 
+document.title = "Merkle Tree Visualizer 🌳";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
